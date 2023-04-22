dd60de74eea95a909c3fac2ede2b3063cb14c231
Marcelino de la Cruz <marcelino.delacruz@urjc.es>
2023-04-22 16:50:02 +0000

    version 1.3

--- R/check.ventana.R
+++ R/check.ventana.R
@@ -1,23 +1,30 @@
-check.ventana <-
-function(ventana){
+check.ventana <- 
+function (ventana) 
+{
     malos1 <- NULL
-    malos2<- NULL
-    for( i in 1:length(ventana$bdry)){
-       test <- try(owin(poly=ventana$bdry[[i]]), silent=TRUE)
-       if(class(test)=="try-error"){
-            malos1<- c(malos1, i) # anota en que subpoligono hay un error
-            # intenta darle la vuelta a las coordenadas a ver si eso lo resuleve
-            test2 <- try( owin(poly=lapply(ventana$bdry[[i]], rev), silent=TRUE))
-              if(class(test2)!="try-error"){
-                   # si al dar la vuelta a las cooredenadas se arregla, grabamos las nuevas coordenadas
-                   ventana$bdry[[i]] <-lapply(ventana$bdry[[i]], rev)
-	        } else malos2<- c(malos2, i)
-	}
-     }
-   attr(ventana, "corrected")  <-malos1
-   attr(ventana, "not.corrected") <- malos2
-   if(!is.null(malos1)) cat( length(malos1), "problematic polygon(s) detected \n \n")
-   if(!is.null(malos1) & is.null(malos2)) cat("all problematic polygons have been repared\n \n")
-   if(!is.null(malos2)) cat(length(malos2), "problematic polygon(s) could not been repared\n \n")
-   return(ventana)
+    malos2 <- NULL
+    for (i in 1:length(ventana$bdry)) {
+        xy <- list(x = ventana$bdry[[i]]$x, y= ventana$bdry[[i]]$y)
+       test <- try(owin(poly=xy), silent=TRUE)
+       if(inherits (test, "try-error")){
+            malos1 <- c(malos1, i)
+            test2 <- try(owin(poly = lapply(xy, rev), silent = TRUE))
+             if(!inherits (test2, "try-error")){
+                ventana$bdry[[i]] <- owin(poly = lapply(xy, rev))$bdry
+            }  else{
+ 	       malos2 <- c(malos2, i)
+		ventana$bdry[[i]]$area <- NULL
+	        ventana$bdry[[i]]$hole <- NULL
+               }
+        }
+    }
+    attr(ventana, "corrected") <- malos1
+    attr(ventana, "not.corrected") <- malos2
+    if (!is.null(malos1)) 
+        cat(length(malos1), "problematic polygon(s) detected \n \n")
+    if (!is.null(malos1) & is.null(malos2)) 
+        cat("all problematic polygons have been repared\n \n")
+    if (!is.null(malos2)) 
+        cat(length(malos2), "problematic polygon(s) could not been repared\n \n")
+    return(ventana)
 }
--- inst/doc/Readshapefiles.R
+++ inst/doc/Readshapefiles.R
@@ -18,116 +18,124 @@ sversion <- read.dcf(file = system.file("DESCRIPTION", package = "overlapptest")
 
 
 ###################################################
-### code chunk number 3: Readshapefiles.Rnw:104-106
+### code chunk number 3: Readshapefiles.Rnw:103-104
 ###################################################
-library(rgdal)
-library(maptools)
+library(sf)
 
 
 ###################################################
-### code chunk number 4: Readshapefiles.Rnw:110-111 (eval = FALSE)
+### code chunk number 4: Readshapefiles.Rnw:108-109 (eval = FALSE)
 ###################################################
-## x <- readOGR("mydata.shp")
+## x <- st_read("mydata.shp")
 
 
 ###################################################
-### code chunk number 5: Readshapefiles.Rnw:116-117 (eval = FALSE)
+### code chunk number 5: Readshapefiles.Rnw:114-115 (eval = FALSE)
 ###################################################
 ## class(x)
 
 
 ###################################################
-### code chunk number 6: Readshapefiles.Rnw:128-129
+### code chunk number 6: Readshapefiles.Rnw:125-126
 ###################################################
 dire<-getwd()
 
 
 ###################################################
-### code chunk number 7: Readshapefiles.Rnw:132-134
+### code chunk number 7: Readshapefiles.Rnw:129-131
 ###################################################
 setwd(system.file("shapes", package="overlapptest"))
-Androsace <- readOGR("Androsace.shp", verbose=FALSE)
+Androsace <- st_read("Androsace.shp", quiet =TRUE)
 
 
 ###################################################
-### code chunk number 8: Readshapefiles.Rnw:136-137
+### code chunk number 8: Readshapefiles.Rnw:133-134
 ###################################################
 class(Androsace)
 
 
 ###################################################
-### code chunk number 9: Readshapefiles.Rnw:139-140
+### code chunk number 9: Readshapefiles.Rnw:136-137
 ###################################################
 setwd(dire)
 
 
 ###################################################
-### code chunk number 10: Readshapefiles.Rnw:148-150
+### code chunk number 10: Readshapefiles.Rnw:144-145
 ###################################################
-library(maptools)
 library(spatstat.geom)
 
 
 ###################################################
-### code chunk number 11: Readshapefiles.Rnw:159-160
+### code chunk number 11: Readshapefiles.Rnw:154-155
 ###################################################
 spatstat.geom::spatstat.options(fixpolygons=FALSE)
 
 
 ###################################################
-### code chunk number 12: Readshapefiles.Rnw:172-173
+### code chunk number 12: Readshapefiles.Rnw:169-170
 ###################################################
-Androsace<- as(Androsace, "owin")
+Androsace <- as.owin(Androsace, check_polygons=FALSE)
 
 
 ###################################################
-### code chunk number 13: Readshapefiles.Rnw:181-182 (eval = FALSE)
+### code chunk number 13: Readshapefiles.Rnw:178-179 (eval = FALSE)
 ###################################################
 ## library(overlapptest)
 
 
 ###################################################
-### code chunk number 14: Readshapefiles.Rnw:196-219
+### code chunk number 14: Readshapefiles.Rnw:193-226
 ###################################################
-check.ventana <-
-function(ventana){
+
+check.ventana <- 
+function (ventana) 
+{
     malos1 <- NULL
-    malos2<- NULL
-    for( i in 1:length(ventana$bdry)){
-       test <- try(owin(poly=ventana$bdry[[i]]), silent=TRUE)
-       if(class(test)=="try-error"){
-            malos1<- c(malos1, i) # anota en que subpoligono hay un error
-            # intenta darle la vuelta a las coordenadas a ver si eso lo resuleve
-            test2 <- try( owin(poly=lapply(ventana$bdry[[i]], rev), silent=TRUE))
-              if(class(test2)!="try-error"){
-                   # si al dar la vuelta a las cooredenadas se arregla, grabamos las nuevas coordenadas
-                   ventana$bdry[[i]] <-lapply(ventana$bdry[[i]], rev)
-	        } else malos2<- c(malos2, i)
-	}
-     }
-   attr(ventana, "corrected")  <-malos1
-   attr(ventana, "not.corrected") <- malos2
-   if(!is.null(malos1)) cat( length(malos1), "problematic polygon(s) detected \n \n")
-   if(!is.null(malos1) & is.null(malos2)) cat("all problematic polygons have been repared\n \n")
-   if(!is.null(malos2)) cat(length(malos2), "problematic polygon(s) could not been repared\n \n")
-   return(ventana)
+    malos2 <- NULL
+    for (i in 1:length(ventana$bdry)) {
+        xy <- list(x = ventana$bdry[[i]]$x, y= ventana$bdry[[i]]$y)
+       test <- try(owin(poly=xy), silent=TRUE)
+       if(inherits (test, "try-error")){
+            malos1 <- c(malos1, i)
+            test2 <- try(owin(poly = lapply(xy, rev), silent = TRUE))
+             if(!inherits (test2, "try-error")){
+                ventana$bdry[[i]] <- owin(poly = lapply(xy, rev))$bdry
+            }  else{
+ 	       malos2 <- c(malos2, i)
+		ventana$bdry[[i]]$area <- NULL
+	        ventana$bdry[[i]]$hole <- NULL
+               }
+        }
+    }
+    attr(ventana, "corrected") <- malos1
+    attr(ventana, "not.corrected") <- malos2
+    if (!is.null(malos1)) 
+        cat(length(malos1), "problematic polygon(s) detected \n \n")
+    if (!is.null(malos1) & is.null(malos2)) 
+        cat("all problematic polygons have been repared\n \n")
+    if (!is.null(malos2)) 
+        cat(length(malos2), "problematic polygon(s) could not been repared\n \n")
+    return(ventana)
 }
 
 
+
+
 ###################################################
-### code chunk number 15: Readshapefiles.Rnw:222-223
+### code chunk number 15: Readshapefiles.Rnw:229-230
 ###################################################
 Androsace <- check.ventana(Androsace)
 
 
 ###################################################
-### code chunk number 16: Readshapefiles.Rnw:229-230
+### code chunk number 16: Readshapefiles.Rnw:236-237
 ###################################################
 options(width=80)
 
 
 ###################################################
-### code chunk number 17: Readshapefiles.Rnw:232-233
+### code chunk number 17: Readshapefiles.Rnw:239-240
 ###################################################
 attributes (Androsace)
 
